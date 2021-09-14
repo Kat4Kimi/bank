b2beb63125be288c2e5c80766d8fda2f21b6018c
Kat Athay <89493136+Kat4Kimi@users.noreply.github.com>
2021-09-13 20:04:51 -0600

    navbar and accounts js

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
+  before_action :authenticate_user!
 end
+
+
+# Everything inherits from application controller
